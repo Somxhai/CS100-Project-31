e9f45e049715cc50f7fca7357d9ee2b4f85620d8
= <=>
2023-12-04 00:16:13 +0700

    change profile onhover to click and fix some issues with color

--- logic/index.js
+++ logic/index.js
@@ -1,51 +1,73 @@
+const items = document.querySelectorAll(".item");
+const navHome = document.querySelector("nav a.home");
+const navActivity = document.querySelector("nav a.activity");
+const navH1 = document.querySelector("nav h1");
+
+const onProfileSelect = (item) => {
+  const color = item.getAttribute("data-color");
+  const textColor = item.getAttribute("data-text-color");
+  document.documentElement.style.setProperty("--body-bg", `var(${color})`);
+  document.getElementById("Greetings").style.color = `var(${textColor})`;
+  document.getElementById("Code31").style.color = `var(${textColor})`;
+  document.documentElement.style.setProperty(
+    "--greetings-text-color",
+    `var(${textColor})`,
+  );
+  document.documentElement.style.setProperty(
+    "--code31-text-color",
+    `var(${textColor})`,
+  );
+  navHome.style.color = `var(${textColor})`;
+  navActivity.style.color = `var(${textColor})`;
+  navH1.style.color = `var(${textColor})`;
+};
+
+const onProfileUnSelect = () => {
+  document.documentElement.style.setProperty("--body-bg", "#f5f5f5");
+  document.getElementById("Greetings").style.color = "";
+  document.getElementById("Code31").style.color = "";
+  document.documentElement.style.removeProperty("--greetings-text-color");
+  document.documentElement.style.removeProperty("--code31-text-color");
+  navHome.style.color = "var(--body-text)";
+  navActivity.style.color = "var(--body-text)";
+  navH1.style.color = "var(--body-text)";
+};
 document.addEventListener("DOMContentLoaded", function () {
-  const items = document.querySelectorAll(".item");
-  const navHome = document.querySelector("nav a.home");
-  const navActivity = document.querySelector("nav a.activity");
-  const navH1 = document.querySelector("nav h1");
   items.forEach((item) => {
-    item.addEventListener("mouseover", function () {
-      const color = this.getAttribute("data-color");
-      const textColor = this.getAttribute("data-text-color");
-      document.documentElement.style.setProperty("--body-bg", `var(${color})`);
-      document.getElementById("Greetings").style.color = `var(${textColor})`;
-      document.getElementById("Code31").style.color = `var(${textColor})`;
-      document.documentElement.style.setProperty(
-        "--greetings-text-color",
-        `var(${textColor})`,
-      );
-      document.documentElement.style.setProperty(
-        "--code31-text-color",
-        `var(${textColor})`,
-      );
-      navHome.style.color = `var(${textColor})`;
-      navActivity.style.color = `var(${textColor})`;
-      navH1.style.color = `var(${textColor})`;
-    });
-    item.addEventListener("mouseout", function () {
-      document.documentElement.style.setProperty("--body-bg", "#f5f5f5");
-      document.getElementById("Greetings").style.color = "";
-      document.getElementById("Code31").style.color = "";
-      document.documentElement.style.removeProperty("--greetings-text-color");
-      document.documentElement.style.removeProperty("--code31-text-color");
-      navHome.style.color = "var(--body-text)";
-      navActivity.style.color = "var(--body-text)";
-      navH1.style.color = "var(--body-text)";
-    });
-    navHome.addEventListener("mouseover", function () {
-      this.style.color = "var(--body-bg)";
-    });
+    item.setAttribute("active", "false");
 
-    navHome.addEventListener("mouseout", function () {
-      this.style.color = "var(--body-text)";
-    });
+    item.addEventListener("click", (event) => {
+      const descriptionElement = item.querySelector(".description");
+      let itemsList = Array.from(items);
+      if (event.target.getAttribute("active") == "false") {
+        // click to other profile while the previous one is already actived
+        let someActived = itemsList.some(
+          (i) => i.getAttribute("active") == "true",
+        );
+        if (someActived) {
+          items.forEach((x) => {
+            if (x.className != item.className) {
+              x.setAttribute("active", "false");
+              x.style.filter = "grayscale(100%)";
+              const otherProfileDescription = x.querySelector(".description");
+              otherProfileDescription.style.display = "none";
+            }
+          });
+        }
+        // get children where it's has class name .description
+        event.target.setAttribute("active", "true");
+        onProfileSelect(item);
+        item.style.filter = "grayscale(0%)";
+        descriptionElement.style.display = "block";
+      } else {
+        descriptionElement.style.display = "none";
+        item.style.filter = "grayscale(100%)";
 
-    navActivity.addEventListener("mouseover", function () {
-      this.style.color = "var(--body-bg)";
-    });
+        event.target.setAttribute("active", "false");
+        onProfileUnSelect();
+      }
 
-    navActivity.addEventListener("mouseout", function () {
-      this.style.color = "var(--body-text)";
+      // set grayscale for every
     });
   });
 });
