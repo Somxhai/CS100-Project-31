f1cdf6479e7c30dbee24ee8ca2b218feb639aaff
= <=>
2023-12-05 00:33:10 +0700

    not using anymore

--- backend/validation/validation.js
+++
@@ -1,9 +0,0 @@
-const spaceBetween = (str) => {
-  const regex = /^(\S+\s{1}\S+)$/;
-  return regex.test(str);
-};
-
-function nameValidation(name) {
-  if (!name) return { result: "", ok: false };
-  if (spaceBetween(name)) return { result: name, ok: true };
-}
