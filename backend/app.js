25000102794e70a61254a70ad9c5ed201b167388
= <=>
2023-12-06 04:42:26 +0700

    add hook to api

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,17 @@ export const app = Fastify({
   logger: true,
 });
 // register
+
+app.addHook("onRequest", (request, reply, done) => {
+  reply.header("Access-Control-Allow-Origin", "*");
+  reply.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS",
+  );
+  reply.header("Access-Control-Allow-Headers", "*");
+  done();
+});
+
 app.register(cors);
 app.register(multipart, {
   limits: {
