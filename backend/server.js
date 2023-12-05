a193d55c74994afb22a3d1df9a778cd7ffa12f63
= <=>
2023-12-06 04:26:28 +0700

    change port to 3030

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import { app } from "./app.js";
 const start = async () => {
   try {
-    await app.listen({ port: 8000, host: "0.0.0.0" });
+    await app.listen({ port: 3030, host: "0.0.0.0" });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
